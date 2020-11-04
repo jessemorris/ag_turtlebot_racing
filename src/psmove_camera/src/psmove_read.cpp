4a4e71b1436574d75242218e99ab6d61a9d2fe06
Oliver Fleming <oliver.fleming1998@gmail.com>
2020-11-04 17:00:57 +1100

    creating arguments

--- src/psmove_camera/src/psmove_read.cpp
+++ src/psmove_camera/src/psmove_read.cpp
@@ -26,13 +26,13 @@ int main(int argc, char **argv)
     n.getParam("psmove_camera/image_topic", output_video_topic);
 
     int device_id;
-    n.getParam("psmove_camera/device_id", device_id);
+    n.getParam("psmove_camera/cam_source", device_id);
 
-    int cam_source;
-    n.getParam("psmove_camera/cam_source", cam_source);
+    // int cam_source;
+    // n.getParam("psmove_camera/cam_source", cam_source);
 
-    std::cout << "\ntesting:" << cam_source << "\n" << std::endl;
-    device_id = cam_source;
+    // std::cout << "\ntesting:" << cam_source << "\n" << std::endl;
+    // device_id = cam_source;
 
 
     image_transport::ImageTransport it(n);
