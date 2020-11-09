b82f046e1f6cf9196d83aaaa5700942de4b9fca3
Talin <talin.roche@gmail.com>
2020-11-10 01:20:59 +1100

    kinda pixel working kinda working -Jesse Morris 2020 (1:20AM)

---
+++ src/object_placement/scripts/main.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+import ros_numpy
+from sensor_msgs.msg import Image, CameraInfo
+import numpy as np
+import math
+
+class ObjectPlacement():
+
+    def __init__(self):
+        self.input_image_topic = rospy.get_param("object_placement/input_camera_topic");
+
+        print(self.input_image_topic)
+
+        rospy.Subscriber(self.input_image_topic,Image,self.imageCallback)
+
+        self.cv_image = None
+        self.principal_point_x = 640/2
+        self.principal_point_y = 480/2
+        self.u0 = self.principal_point_x
+        self.v0 = self.principal_point_y
+        self.fx = 519.566467
+        self.fy = 522.066406
+        self.distortion_array = np.array([0.187523, -0.355696, 0.002719, -0.003771, 0.000000])
+
+        #k1, k2, k3 is radial distortion
+        #k4,k5 is tangential distortion
+        self.k4 = self.distortion_array[0]
+        self.k5 = self.distortion_array[1]
+        self.k1 = self.distortion_array[2]
+        self.k2 = self.distortion_array[3]
+        self.k3 = self.distortion_array[4]
+
+
+
+
+
+    def getPixelCoordinates(self):
+        #p = point in world frame [x,y,z,0]
+        x = 1
+        y = 0
+        z = 0.5
+        p = np.array([x,y,z,0])
+
+        # R = camera rotation matrix
+        # T = camera translation matrix
+        R = np.identity(3)
+
+        T = np.array([[0],[0],[0]])
+
+
+
+        #K = intrinsic matrix
+        K= np.array([[519.566467, 0.000000, 313.837735, 0.000000],
+        [0.000000, 522.066406, 248.386084, 0.000000],
+        [0.000000, 0.000000, 1.000000, 0.000000]])
+
+
+
+
+        #extrinsic_matrix = [R', T'; 0,0,0,1]
+        extrinsic_matrix = np.concatenate((R,T),axis = 1)
+        test = np.array([[0,0,0,1]])
+
+        extrinsic_matrix = np.concatenate((extrinsic_matrix,test))
+
+
+        #
+        # np.array[[R,np.transpose(T)],[0,0,0,1]])
+
+
+        test = np.matmul(K,extrinsic_matrix)
+
+        camera_frame = np.matmul(np.matmul(K,extrinsic_matrix),np.transpose(p))
+        print(camera_frame)
+
+
+        #these are the undistored points
+        u = np.divide(camera_frame[0],camera_frame[2])
+        v = np.divide(camera_frame[1],camera_frame[2])
+
+
+
+
+        #u0 = principal_point x
+        #v0 = principal_point y
+
+        # fx = focal length x
+        # fy = focal length y
+
+        x = (u-self.u0)/self.fx
+        y = (v-self.v0)/self.fy
+
+        r = math.sqrt(x**2+y**2)
+
+        #k1, k2, k3 is radial distortion
+        #k4,k5 is tangential distortion
+        xd = x*(1+self.k1*r**2 +self.k2*r**4 + self.k3*r**6) + 2*self.k4*x*y+self.k5*(r**2+2*x**2);
+        yd = y*(1+self.k1*r**2 +self.k2*r**4 + self.k3*r**6)+self.k4*(r**2+2*y**2)+2*self.k5*x*y;
+
+        #distorted pixel coordinates
+        ud = self.fx*xd + self.u0;
+        vd= self.fy*yd + self.v0;
+
+        print(vd)
+        print(vd)
+
+
+
+
+    def imageCallback(self,data):
+
+        # returns image as a numpy array
+        self.cv_image = ros_numpy.numpify(data)[... , :3][...,::-1]
+        print(self.cv_image.shape)
+
+        # for each of the objects in the world frame
+        self.getPixelCoordinates()
+
+
+
+
+if __name__ == '__main__':
+    # listener()
+    rospy.init_node('object_placement')
+    obj = ObjectPlacement()
+
+    rospy.spin()
